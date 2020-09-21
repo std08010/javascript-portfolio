e818eeb2360336cb1c7099946f7d37f20f57cc17
acipi <haris.tsipis@gmail.com>
2020-09-21 19:25:34 +0300

    array equality function + Symbols testing

--- hello.js
+++ hello.js
@@ -6,3 +6,8 @@ let t = Symbol("sym_x");
 
 console.log(s.toString());
 console.log(t.toString());
+
+s = Symbol.for("shared");
+t = Symbol.for("shared");
+console.log(s === t);
+console.log(Symbol.keyFor(t));
